870ee140c07354faadc52879d7196f5d26d78155
anhuong541 <nguyenxuananhuong541@gmail.com>
2024-08-01 17:40:45 +0700

    set up server

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+const cors = require("cors");
+const port = 3456;
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: ["http://localhost:3001"],
+  optionsSuccessStatus: 200,
+  methods: "GET,POST,PUT,DELETE",
+  //   allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", async (req: Request, res: Response) => {
+  console.log("it runn!!!!");
+  res.json();
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
