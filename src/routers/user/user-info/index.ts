4bd1043149ae2704122449f30d0c57071796dcc1
anhuong541 <nguyenxuananhuong541@gmail.com>
2024-08-06 11:44:15 +0700

    chore: add error at user info api

--- src/routers/user/user-info/index.ts
+++ src/routers/user/user-info/index.ts
@@ -3,8 +3,14 @@ import { getUserDataById } from "../../../lib/firebase-func";
 
 export default async function TakeUserInfoHandler(req: Request, res: Response) {
   const uid = req.params.userid ?? "";
-  const data = await getUserDataById(uid);
-  return res
-    .status(200)
-    .json({ status: "success", message: "get user data success", data });
+  try {
+    const data = await getUserDataById(uid);
+    return res
+      .status(200)
+      .json({ status: "success", message: "get user data success", data });
+  } catch (error) {
+    return res
+      .status(404)
+      .json({ status: "fail", message: "missing uid or something", error });
+  }
 }
