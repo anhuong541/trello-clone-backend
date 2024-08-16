be51b78e75596bc756b1a024c7180fee71bc2d3a
anhuong541 <nguyenxuananhuong541@gmail.com>
2024-08-16 11:06:57 +0700

    add console.log cookies

--- src/lib/auth-action.ts
+++ src/lib/auth-action.ts
@@ -2,10 +2,13 @@ import { Response } from "express";
 import config from "../config";
 
 export const sendUserSession = (res: Response, token: string) => {
-  res.cookie("user_session", token, {
+  console.log({ token, env: config.env });
+  const data = res.cookie("user_session", token, {
     httpOnly: true,
     secure: config.env,
     maxAge: 2 * 60 * 60 * 1000, // two hours
     path: "/",
   });
+
+  console.log({ data });
 };
