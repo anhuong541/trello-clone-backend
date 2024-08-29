ca25063d1c0299d6ce96606c4215798b23447423
anhuong541 <nguyenxuananhuong541@gmail.com>
2024-08-29 15:51:27 +0700

    update new module.export

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("module-alias/register");
-const index_1 = require("./ws/index");
-const index_2 = require("./web/index");
+// import { httpServer, wssport } from "./ws/index";
+const index_1 = require("./web/index");
 // Start the server
-index_1.httpServer.listen(index_1.wssport, () => {
-    console.log(`Websocket is listening on ${index_1.wssport}`);
+// module.exports = httpServer.listen(wssport, () => {
+//   console.log(`Websocket is listening on ${wssport}`);
+// });
+module.exports = index_1.app.listen(index_1.port, () => {
+    console.log(`express is listen to port: ${index_1.port}`);
 });
-index_2.app.listen(index_2.port, () => {
-    console.log(`express is listen to port: ${index_2.port}`);
-});
-module.exports = index_2.app;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import "module-alias/register";
 
-import { httpServer, wssport } from "./ws/index";
+// import { httpServer, wssport } from "./ws/index";
 import { app, port } from "./web/index";
 
 // Start the server
-httpServer.listen(wssport, () => {
-  console.log(`Websocket is listening on ${wssport}`);
-});
+// module.exports = httpServer.listen(wssport, () => {
+//   console.log(`Websocket is listening on ${wssport}`);
+// });
 
-app.listen(port, () => {
+module.exports = app.listen(port, () => {
   console.log(`express is listen to port: ${port}`);
 });
-
-module.exports = app;
