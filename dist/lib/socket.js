c6e02fe6ba23ddcdfea2dae419f088af63b6a719
huong541 <nguyenxuananhuong541@gmail.com>
2024-09-04 23:29:03 +0700

    remove unused code

--- dist/lib/socket.js
+++ dist/lib/socket.js
@@ -1,11 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.socket = exports.ablyRealtime = void 0;
+exports.ablyRealtime = void 0;
 const tslib_1 = require("tslib");
-const socket_io_client_1 = require("socket.io-client");
 const ably_1 = tslib_1.__importDefault(require("ably"));
 const config_1 = tslib_1.__importDefault(require("./../config"));
 exports.ablyRealtime = new ably_1.default.Realtime(config_1.default.ablyKey);
-exports.socket = (0, socket_io_client_1.io)("http://localhost:8080");
-// move socket to context for global state manemanet
 //# sourceMappingURL=socket.js.map
--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,8 +1,4 @@
-import { io } from "socket.io-client";
 import Ably from "ably";
 import config from "./../config";
 
 export const ablyRealtime = new Ably.Realtime(config.ablyKey);
-
-export const socket = io("http://localhost:8080");
-// move socket to context for global state manemanet
